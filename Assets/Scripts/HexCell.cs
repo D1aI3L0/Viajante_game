0b91d00c9184254449a26135662a9929dc3265ef
D1aI3L0 <tochagamer2801@gmail.com>
2025-01-04 19:33:17 +0300

    Added colors mixing and cells heights

--- Assets/Scripts/HexCell.cs
+++ Assets/Scripts/HexCell.cs
@@ -2,9 +2,31 @@ using UnityEngine;
 
 public class HexCell : MonoBehaviour 
 {
+    public RectTransform uiRect;
     public HexCoordinates coordinates;
     
     public Color color;
+
+    private int elevation;
+
+    public int Elevation 
+    {
+		get 
+        {
+			return elevation;
+		}
+		set 
+        {
+			elevation = value;
+			Vector3 position = transform.localPosition;
+			position.y = value * HexMetrics.elevationStep;
+			transform.localPosition = position;
+
+            Vector3 uiPosition = uiRect.localPosition;
+			uiPosition.z = elevation * -HexMetrics.elevationStep;
+			uiRect.localPosition = uiPosition;
+		}
+	}
 
     [SerializeField]
 	HexCell[] neighbors;
@@ -20,5 +42,14 @@ public class HexCell : MonoBehaviour
         cell.neighbors[(int)direction.Opposite()] = this;
 	}
 
+    public HexEdgeType GetEdgeType (HexDirection direction) 
+    {
+		return HexMetrics.GetEdgeType(elevation, neighbors[(int)direction].elevation);
+	}
+
+    public HexEdgeType GetEdgeType (HexCell otherCell) 
+    {
+		return HexMetrics.GetEdgeType(elevation, otherCell.elevation);
+	}
 
 }
